04961dbb064124c23320c3b0b24a4b4440487e0e
hp <hp@FTI-LAB03-CAD1>
2025-09-08 11:46:24 +0700

    pratikum alpro pekan 1

---
+++ Prakalpro_2025_B_2511532026/src/pekan1/Helloworld.java
@@ -0,0 +1,20 @@
+package pekan1;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		//program pertama
+		/*komentar
+		 * dua baris atau lebih
+		 * pertemuan 1 praktikum alpro
+		 */
+		System.out.println ("Hello World") ;
+		System.out.println ();
+		System.out.println ("program ini menghasilkan") ;
+		System.out.println ("empat baris output");
+		System.out.println ("/ \\ // \\\\ /// \\\\ ");
+		System.out.println ("This"+"program prints a\n"+ "quote from the gettysburg address.");
+		// TODO Auto-generated method stub
+	}
+
+}
