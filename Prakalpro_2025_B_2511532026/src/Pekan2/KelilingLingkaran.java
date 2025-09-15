f9b1c943d13dd8b5f0a42ba57492b77d21c0f6b2
hp <hp@FTI-LAB03-CAD1>
2025-09-15 11:41:33 +0700

    Pratikum pekan 2

---
+++ Prakalpro_2025_B_2511532026/src/Pekan2/KelilingLingkaran.java
@@ -0,0 +1,13 @@
+package Pekan2;
+
+public class KelilingLingkaran {
+
+	public static void main(String[] args) {
+		final double PI = 3.14; /* Definisi konstanta */
+		double radius = 30; /* Deklarasi variabel */
+		 System.out.println("keliling = " + 2 * PI * radius);
+		
+
+	}
+
+}
