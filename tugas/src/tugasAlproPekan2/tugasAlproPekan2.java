690f4b6aa5b42ddfbaf883d3ae17dc28728e224c
Dhinotea <Dhinotea@Force-46>
2025-09-21 14:26:20 +0700

    tugasAlproPekan2

---
+++ tugas/src/tugasAlproPekan2/tugasAlproPekan2.java
@@ -0,0 +1,22 @@
+package tugasAlproPekan2;
+
+public class tugasAlproPekan2 {
+
+	public static void main(String[] args) {
+		// Deklarasi variabel dengan 4 tipe data dasar
+		int jumlahBuku = 26;
+		float hargaBuku = 130.50f;
+		char kategori = 'b';
+		boolean tersedia = false;
+		
+		// Menampilkan output ke layar
+		System.out.println("===== Data Buku =====");
+		System.out.println("Jumlah Buku : " + jumlahBuku);
+		System.out.println("Harga Buku  : Rp" + hargaBuku);
+		System.out.println("Kategori    : " + kategori);
+		System.out.println("Tersedia?   : " + tersedia);
+		// TODO Auto-generated method stub
+
+	}
+
+}
