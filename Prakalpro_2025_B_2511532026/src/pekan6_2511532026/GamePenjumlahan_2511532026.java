d00d3aafad3836309c27ad61c83f076fcaa9d1b1
Aryahiyahulf <aryahiyahulf@gmail.com>
2025-11-03 17:10:35 +0700

    Add files via upload

---
+++ Prakalpro_2025_B_2511532026/src/pekan6_2511532026/GamePenjumlahan_2511532026.java
@@ -0,0 +1,10 @@
+package pekan6_2511532026;
+
+public class GamePenjumlahan_2511532026 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Prakalpro_2025_B_2511532026/src/pekan6_2511532026/Sentinel1Loop_2511532026.java
@@ -0,0 +1,10 @@
+package pekan6_2511532026;
+
+public class Sentinel1Loop_2511532026 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
